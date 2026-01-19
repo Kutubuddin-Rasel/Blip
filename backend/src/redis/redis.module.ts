80ae84bb94b4c4807e152ac9dfb4e086354d975d
Kutubuddin-Rasel <raselvai257@gmail.com>
2026-01-19 21:49:26 +0600

    feat(redis): enhance Redis service with connection management and event handling

--- backend/src/redis/redis.module.ts
+++ backend/src/redis/redis.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import { ConfigModule } from '@nestjs/config';
 
+@Global()
 @Module({
+  imports: [ConfigModule],
   providers: [RedisService],
+  exports: [RedisService],
 })
 export class RedisModule {}
--- backend/src/redis/redis.service.ts
+++ backend/src/redis/redis.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+  private redis: Redis;
+  private isConnected = false;
+  constructor(private readonly configService: ConfigService) {}
+
+  onModuleInit() {
+    try {
+      this.redis = new Redis({
+        host: this.configService.getOrThrow('REDIS_HOST'),
+        port: this.configService.getOrThrow('REDIS_PORT'),
+        password: this.configService.getOrThrow('REDIS_PASSWORD'),
+        db: this.configService.getOrThrow('REDIS_DB'),
+      });
+
+      this.redis.on('error', (error) => {
+        console.warn('Redis connection error: ', error.message);
+        this.isConnected = false;
+      });
+
+      this.redis.on('connect', () => {
+        console.log('Redis connected successfully');
+        this.isConnected = true;
+      });
+
+      this.redis.on('ready', () => {
+        console.log('Redis ready for operations');
+        this.isConnected = true;
+      });
+
+      this.redis.on('reconnecting', () => {
+        console.log('Redis reconnecting ....');
+      });
+
+      this.redis.on('end', () => {
+        console.log('Redis connection ended');
+        this.isConnected = false;
+      });
+    } catch (error: unknown) {
+      console.warn('Redis error:', error);
+      this.isConnected = false;
+    }
+  }
+
+  async onModuleDestroy() {
+    if (this.redis) {
+      await this.redis.quit();
+      console.log('Redis disconnected');
+    }
+  }
+}
